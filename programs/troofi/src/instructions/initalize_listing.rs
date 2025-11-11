5f17829e4a10405138ff135176a26f236a7d3aba
Bijoy99roy <bijoy2014amp@gmail.com>
2025-11-11 15:21:08 +0000

    chore: Move initialization to instructions

--- programs/troofi/src/instructions/initalize_listing.rs
+++ programs/troofi/src/instructions/initalize_listing.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use mpl_core::instructions::TransferV1CpiBuilder;
 
 use crate::Listing;
 
@@ -6,7 +7,7 @@ use crate::Listing;
 pub struct InitalizeListing<'info> {
     #[account(mut)]
     pub seller: Signer<'info>,
-
+    /// CHECK: The core asset account
     #[account(mut)]
     pub asset: AccountInfo<'info>,
 
@@ -18,8 +19,29 @@ pub struct InitalizeListing<'info> {
         bump
     )]
     pub listing_pda: Account<'info, Listing>,
-
+    /// CHECK: mpl-core program
     pub mpl_core_program: AccountInfo<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitalizeListing<'info> {
+    pub fn initialize(&mut self, price: u64, bump: u8) -> Result<()> {
+        let listing = &mut self.listing_pda;
+
+        listing.seller = self.seller.key();
+        listing.asset = self.asset.key();
+        listing.price = price;
+        listing.bump = bump;
+
+        // CPI to mpl core to transfer the asset to listing pda
+        TransferV1CpiBuilder::new(&self.mpl_core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .authority(Some(&self.seller.to_account_info()))
+            .new_owner(&self.listing_pda.to_account_info())
+            .system_program(Some(&self.system_program.to_account_info()))
+            .invoke()?;
+
+        Ok(())
+    }
 }
--- programs/troofi/src/lib.rs
+++ programs/troofi/src/lib.rs
@@ -9,25 +9,11 @@ declare_id!("4hXnADEUHeDLGZtiPiESaRoutUPRHX2NDruhNr2W6rTc");
 
 #[program]
 pub mod troofi {
-    use mpl_core::instructions::{TransferV1Builder, TransferV1CpiBuilder};
 
     use super::*;
 
     pub fn initialize_listing(ctx: Context<InitalizeListing>, price: u64) -> Result<()> {
-        let listing = &mut ctx.accounts.listing_pda;
-
-        listing.seller = ctx.accounts.seller.key();
-        listing.asset = ctx.accounts.asset.key();
-        listing.price = price;
-        listing.bump = ctx.bumps.listing_pda;
-
-        // CPI to mpl core to transfer the asset to listing pda
-        TransferV1CpiBuilder::new(&ctx.accounts.mpl_core_program.to_account_info())
-            .asset(&ctx.accounts.asset.to_account_info())
-            .authority(Some(&ctx.accounts.seller.to_account_info()))
-            .new_owner(&ctx.accounts.listing_pda.to_account_info())
-            .system_program(Some(&ctx.accounts.system_program.to_account_info()))
-            .invoke()?;
+        ctx.accounts.inialize(price, ctx.bumps.listing_pda)?;
         Ok(())
     }
 }
